9d66cb4655b9248f8967c0af0fedd63910ac2a83
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-06 14:46:04 +0200

    '1'

--- Zjazd1_05_04_25/2_dane/2_otodom.py
+++ Zjazd1_05_04_25/2_dane/2_otodom.py
@@ -3,4 +3,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('otodom.csv')
-print(df)
+print(df.head().to_string())
+print(df.describe().T.round(2).to_string())
+
+sns.heatmap(df.iloc[:,1:].corr(), annot=True)
+plt.show()
+
+sns.histplot(df.cena)
+plt.show()
+
+plt.scatter(df.powierzchnia, df.cena)
+plt.show()
+q1 = df.describe().loc["25%", 'cena']
+q3 = df.describe().loc["75%", 'cena']
+
+df1 = df[ (df.cena >= q1) & (df.cena <= q3)  ]
+sns.histplot(df1.cena)
+plt.show()
+
