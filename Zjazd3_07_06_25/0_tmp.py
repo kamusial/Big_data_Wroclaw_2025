4cf7c0f7417a9d0e28020ace78459d484c8fa9b0
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-06-07 08:54:01 +0200

    '1'

--- Zjazd3_07_06_25/0_tmp.py
+++ Zjazd3_07_06_25/0_tmp.py
@@ -8,3 +8,11 @@ print(komunikat[9:19])   # slowo 'zalogowany'
 nowy_komunikat = 'siema ' + komunikat[9:19]
 print(nowy_komunikat)   # 'siema zalogowany
 
+for i in range(len(nowy_komunikat)):
+    print(f'Powtorzenie {i+1}: litera o indeksie {i}, to {nowy_komunikat[i]}')
+
+if nowy_komunikat[-1] != 'a':
+    print('Witam Pana')
+else:
+    print('Witam Panią')
+
