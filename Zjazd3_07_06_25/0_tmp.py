0d99ec92f738e53e7c4efa271ae03c6c469c19c1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-06-07 08:47:00 +0200

    '1'

--- Zjazd3_07_06_25/0_tmp.py
+++ Zjazd3_07_06_25/0_tmp.py
@@ -1,1 +1,10 @@
-print('Siema')
+print('SiemaMerito')
+
+komunikat = 'Zostales zalogowany i juz'
+print(komunikat)
+print(komunikat[4])   # litera 'a'
+print(komunikat[9:19])   # slowo 'zalogowany'
+
+nowy_komunikat = 'siema ' + komunikat[9:19]
+print(nowy_komunikat)   # 'siema zalogowany
+
