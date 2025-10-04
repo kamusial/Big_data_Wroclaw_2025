973aa84fc43ed97dbb3ac24e7a86aa7996ec0852
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-10-04 10:13:21 +0200

    '1'

--- Zjazd3_07_06_25/16_logistyczna_regresja.py
+++ Zjazd3_07_06_25/16_logistyczna_regresja.py
@@ -46,3 +46,13 @@ print('Zmiana danych')
 df1 = df.query("outcome==0").sample(n=500)
 df2 = df.query("outcome==1").sample(n=500)
 df3 = pd.concat([df1, df2])
+
+X = df3.iloc[:, :-1]
+y = df3.outcome
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+print('\nLogistic Regression - po zmianie')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
