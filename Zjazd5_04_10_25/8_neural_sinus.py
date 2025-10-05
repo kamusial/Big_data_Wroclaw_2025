b6565f87201dde1a19a32d4e6a656abd73d0c5d1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-10-05 11:32:56 +0200

    '1'

--- Zjazd5_04_10_25/8_neural_sinus.py
+++ Zjazd5_04_10_25/8_neural_sinus.py
@@ -24,7 +24,30 @@ plt.xticks(
 )
 plt.show()
 
+model = Sequential()
+model.add(Dense(1, input_dim=1, activation='linear'))
+model.add(Dense(4, activation='linear'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(1, activation='linear'))
 
+model.compile(optimizer='rmsprop', loss='mse')
 
+result = model.fit(x, y, epochs=200)
+
+y_pred = model.predict(x)
+plt.scatter(x, y)
+plt.plot(x, y_pred, c='r')
+plt.title('Wykres funkcji sinus')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.legend()
+plt.axhline(0, color='black', linewidth=0.5)  # Oś X
+plt.axvline(0, color='black', linewidth=0.5)  # Oś Y
+plt.xticks(
+    ticks=[-2*np.pi, -3*np.pi/2, -np.pi, -np.pi/2, 0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
+    labels=['-2π', '-3π/2', '-π', '-π/2', '0', 'π/2', 'π', '3π/2', '2π']
+)
+plt.show()
 
 
